b25dbc771fccd8e7edd2c6056552d34146ab1f42
Alan Stewart <alankstewart@gmail.com>
2017-08-31 12:00:35 +1000

    Allow lower case input

--- src/main/java/alankstewart/fx/FxCalculator.java
+++ src/main/java/alankstewart/fx/FxCalculator.java
@@ -26,7 +26,7 @@ public class FxCalculator {
         try {
             final Pairs pairs = new Pairs(readPairsData());
             final Map<String, Integer> currencyData = readCurrencyData();
-            final Pattern p = Pattern.compile("(^[A-Z]{3}) ([0-9]+)(\\.[0-9]+)? in ([A-Z]{3}$)");
+            final Pattern p = Pattern.compile("(^[A-Za-z]{3}) ([0-9]+)(\\.[0-9]+)? in ([A-Za-z]{3}$)");
             final Scanner scanner = new Scanner(System.in);
             while (true) {
                 final String line = scanner.nextLine();
@@ -36,10 +36,10 @@ public class FxCalculator {
 
                 final Matcher m = p.matcher(line);
                 if (m.matches()) {
-                    final String base = m.group(1);
+                    final String base = m.group(1).toUpperCase();
                     final BigDecimal amount = new BigDecimal(m.group(2) +
                             Optional.ofNullable(m.group(3)).orElse(""));
-                    final String terms = m.group(4);
+                    final String terms = m.group(4).toUpperCase();
                     final BigDecimal convertedAmount = pairs.getRate(base, terms)
                             .multiply(amount)
                             .setScale(currencyData.get(terms), RoundingMode.HALF_UP);
@@ -63,14 +63,14 @@ public class FxCalculator {
         return readFile("pairs_rates.dat")
                 .stream()
                 .map(line -> line.split("="))
-                .collect(toMap(p -> p[0], p -> new BigDecimal(p[1])));
+                .collect(toMap(p -> p[0].toUpperCase(), p -> new BigDecimal(p[1])));
     }
 
     private Map<String, Integer> readCurrencyData() {
         return readFile("currency.dat")
                 .stream()
                 .map(line -> line.split("="))
-                .collect(toMap(c -> c[0], c -> Integer.parseInt(c[1])));
+                .collect(toMap(c -> c[0].toUpperCase(), c -> Integer.parseInt(c[1])));
     }
 
     private List<String> readFile(final String fileName) {
